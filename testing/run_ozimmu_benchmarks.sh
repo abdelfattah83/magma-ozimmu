3977742626f9bac9a5b9d21cbd12097c4ab77b76
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2025-01-22 20:51:57 -0500

    add checkpointing to the run script

--- testing/run_ozimmu_benchmarks.sh
+++ testing/run_ozimmu_benchmarks.sh
@@ -1,33 +1,52 @@
 machine=blackwell
 magma_oz=.
 outdir=ozimmu_results
+logfile=$outdir/ozimmu_log.txt
 
 mkdir -p $outdir
+touch    $logfile
 
+alldone=1
 ##################################################################################################################
 ## GEMM on random matrices, three shapes (square, rank-k updates, left-looking cholesky updates)
 nstart=1024
 nstop=20480
 nstep=1024
-niter=5
+niter=4
 
 # square
 for s in 3 6 7 8
 do
    fname=$outdir/dgemm_sq_ozimmu_s${s}_$machine.txt
-   echo "generating " $fname
-   ${magma_oz}/testing_dgemm -N $nstart:$nstop:$nstep --oz $s --niter $niter -l > $fname
+   status=done_dgemm_sq_ozimmu_s${s}_$machine.txt
+   if grep -Fxq "$status" $logfile; then
+       : # nothing to do
+   else
+       # run the test
+       alldone=0
+       echo "generating " $fname
+       ${magma_oz}/testing_dgemm -N $nstart:$nstop:$nstep --oz $s --niter $niter -l > $fname
+       echo $status >> $logfile
+   fi
 done
 
 # rank-k updates (k = 512)
 for s in 3 6 7 8
 do
-   for k in 512
-   do
-       fname=$outdir/dgemm_k${k}_ozimmu_s${s}_$machine.txt
-       echo "generating " $fname
-       ${magma_oz}/testing_dgemm -N $nstart:$nstop:$nstep,$nstart:$nstop:$nstep,${k} --oz $s --niter $niter -l > $fname
-   done
+    for k in 512
+    do
+        fname=$outdir/dgemm_k${k}_ozimmu_s${s}_$machine.txt
+        status=done_dgemm_k${k}_ozimmu_s${s}_$machine.txt
+        if grep -Fxq "$status" $logfile; then
+            : # nothing to do
+        else
+            # run the test
+            alldone=0
+            echo "generating " $fname
+            ${magma_oz}/testing_dgemm -N $nstart:$nstop:$nstep,$nstart:$nstop:$nstep,${k} --oz $s --niter $niter -l > $fname
+            echo $status >> $logfile
+        fi
+    done
 done
 
 # left-looking chol. updates (N = 40960, k = 512)
@@ -37,8 +56,16 @@ Nstop=$((N-K-K))
 for s in 3 6 7 8
 do
     fname=$outdir/dgemm_chol_40k_k${K}_ozimmu_s${s}_${machine}.txt
-    echo "generating " $fname
-    ${magma_oz}/testing_dgemm -N $Nstop:$K:-$K,$K,$K:$Nstop:$K -NT --oz $s --niter $niter -l > $fname
+    status=done_dgemm_chol_40k_k${K}_ozimmu_s${s}_${machine}.txt
+    if grep -Fxq "$status" $logfile; then
+        : # nothing to do
+    else
+        # run the test
+        alldone=0
+        echo "generating " $fname
+        ${magma_oz}/testing_dgemm -N $Nstop:$K:-$K,$K,$K:$Nstop:$K -NT --oz $s --niter $niter -l > $fname
+        echo $status >> $logfile
+    fi
 done
 
 ##################################################################################################################
@@ -50,11 +77,19 @@ niter=1
 
 for s in 8 12 16 18
 do
-   for cond in 1e10 1e20 1e30 1e40
-   do
-	fname=$outdir/dgemm_sq_badly_scaled_Dcond_${cond}_ozimmu_s${s}_$machine.txt
-	echo "generating " $fname
-	${magma_oz}/testing_dgemm -N $nstart:$nstop:$nstep --oz $s --niter $niter -l -c2 --cond $cond > $fname
+    for cond in 1e10 1e20 1e30 1e40
+    do
+        fname=$outdir/dgemm_sq_badly_scaled_Dcond_${cond}_ozimmu_s${s}_$machine.txt
+        status=done_dgemm_sq_badly_scaled_Dcond_${cond}_ozimmu_s${s}_$machine.txt
+        if grep -Fxq "$status" $logfile; then
+            : # nothing to do
+        else
+            # run the test
+            alldone=0
+            echo "generating " $fname
+            ${magma_oz}/testing_dgemm -N $nstart:$nstop:$nstep --oz $s --niter $niter -l -c2 --cond $cond > $fname
+            echo $status >> $logfile
+        fi
     done
 done
 
@@ -73,9 +108,20 @@ do
         for mtx in poev_cluster1
         do
             fname=$outdir/dsyevd_${mtx}_${cond}_ozimmu_s${splits}_${machine}.txt
-            echo "generating " $fname
-            ${magma_oz}/testing_dsyevd_gpu -JV -N $nstart:$nstop:$nstep -c --niter $niter --matrix $mtx --cond $cond --oz $splits > $fname
+            status=done_dsyevd_${mtx}_${cond}_ozimmu_s${splits}_${machine}.txt
+            if grep -Fxq "$status" $logfile; then
+                : # nothing to do
+            else
+                # run the test
+                alldone=0
+                echo "generating " $fname
+                ${magma_oz}/testing_dsyevd_gpu -JV -N $nstart:$nstop:$nstep -c --niter $niter --matrix $mtx --cond $cond --oz $splits > $fname
+                echo $status >> $logfile
+            fi
         done
     done
 done
 
+if [ "$alldone" = "1" ]; then
+  echo "All tests already complete"
+fi
